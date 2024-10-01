5a4639498917c518c80badfa39baa7774040ce5b
Pushkar Kumar Gautam <kumargautampushkar@gmail.com>
2024-10-01 11:58:31 +0530

    fixed code related to `void all_occurance()`

--- Level-1/10_all_occurances.cpp
+++ Level-1/10_all_occurances.cpp
@@ -1,21 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//  This is recursive code to return vector of indices of all occurances
 void all_occurances(const vector <int> &arr, int num, int i, vector <int > &ans){
     if(i == arr.size()) return;
     if(arr[i]==num) ans.push_back(i);
     all_occurances(arr,num,i+1,ans); 
 }
 
+vector <int> all_occ(const vector <int> & arr, int num, int i, int fsf){
+    
+}
+
 int main(){
     int n;
     cin>>n;
-    vector <int> v;
+    vector <int> v(n);
     for(int i = 0; i<n;i++){
         cin>>v[i];
     }
     int num;
-    cin>>n;
+    cin>>num;
 
     vector <int > ans;
     all_occurances(v,num,0,ans);
